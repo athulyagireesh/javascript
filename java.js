3c3ed762eaa238e2b5d123a87262f388f12f3f12
athulyagireesh <athulya@9778#>
2025-10-14 10:18:26 +0530

    javascript

---
+++ java.js
@@ -0,0 +1,56 @@
+// var a=10;
+// console.log(a)
+
+
+var a=20
+console.log(typeof(a))
+
+
+var a='athulya'
+console.log(typeof(a))
+
+
+var a=true
+console.log(typeof(a))
+
+
+var a=Symbol("hloo")
+var b=Symbol("hloo")
+console.log(a==b)
+
+
+var a=("hloo")
+var b=("hloo")
+console.log(a==b)
+
+
+
+
+var a=50n
+var b=70
+let sum=a+BigInt(b);
+console.log(sum)
+
+
+var a=["athulya",21,true,"false"]
+// console.log(typeof(a))
+console.log(a[1])
+
+
+var a={
+    name:"athulya",
+    age:21,
+    place:"kochi",
+    instituition:"synnefo"
+}
+console.log(a)
+
+
+
+var a={
+    name:"athulya",
+    age:21,
+    place:"kochi",
+    instituition:"synnefo"
+}
+console.log(a.place)
---
+++ work.js
@@ -0,0 +1,399 @@
+// let color="yellow"
+
+// switch(color)
+// {
+//     case "red":console.log("stop")
+//     break;
+
+//     case "yellow":console.log("get ready")
+//     break;
+
+//     case "green":console.log("go")
+//     break;
+
+//     default:console.log("invalid")
+//     break;
+// }
+
+
+
+
+// let age =95;
+// if (age < 18) 
+// {
+//   console.log("Not eligible to vote");
+// }
+//  else if (age < 60)
+// {
+//   console.log("Eligible to vote");
+// } 
+// else
+// {
+//   console.log("Eligible (Senior Citizen)");
+// }
+
+
+
+
+// let number = -8;
+// if (number > 0)
+// {
+//     console.log("The number is positive.");
+// } 
+// else if (number < 0) 
+// {
+//     console.log("The number is negative.");
+// } 
+// else
+// {
+//     console.log("The number is zero.");
+// }
+
+
+
+
+// let marks = 95;
+// if (marks >= 90) 
+// {
+//   console.log("Grade: A");
+// } 
+
+// else if (marks >= 75)
+//  {
+//   console.log("Grade: B");
+// }
+
+//  else if (marks >= 60)
+// {
+//   console.log("Grade: C");
+// } 
+
+// else if (marks >= 40) 
+// {
+//   console.log("Grade: D");
+// }
+
+//  else 
+// {
+//   console.log("Grade: F");
+// }
+
+
+
+
+// let number = 3;
+// if (number % 2 == 0)
+// {
+//   console.log("The number is Even");
+// } 
+// else
+// {
+//   console.log("The number is Odd");
+// }
+
+
+
+
+// let a=5;
+// let b=6;
+// let c=50;
+// if(a>b)
+// {
+//     console.log("a is larger")
+// }
+// else if(b>c)
+// {
+//     console.log("b is larger")
+// }
+// else
+// {
+//     console.log("c is larger")
+// }
+
+
+
+
+// let vote=70;
+// if(vote >=18)
+// {
+//     console.log("eligible for vote")
+// }
+// else{
+//     console.log("not eligible")
+// }
+
+
+
+
+// let year =2025;
+// if (year % 4 == 0) 
+// {
+//   console.log(year + " is a leap year.");
+// } 
+// else 
+// {
+//   console.log(year + " is not a leap year.");
+// }
+
+
+
+// let temp =30;
+// if (temp >= 35) 
+// {
+//   console.log("hot temparature");
+// } 
+// else if (temp >= 25) 
+// {
+//   console.log("warm temparature");
+// }
+//  else if (temp >= 15) 
+// {
+//   console.log("pleasant temparature");
+// } 
+// else 
+// {
+//   console.log("cold temparature");
+// }
+
+
+
+
+// let side1=21;
+// let side2=21;
+// let side3=21;
+// if(side1==side2 && side2==side3)
+// {
+//   console.log("equilateral")
+// }
+// else if (side1==side2 && side2!=side3)
+// {
+//   console.log("isosceles")
+// }
+// else
+// {
+//   console.log("scalene")
+// }
+
+
+
+
+
+// for(let i=1;i<=10;i++)
+// {
+//   let rows=" ";
+//   for(let j=1;j<=10;j++)
+//   {
+//     rows+=j+" "
+//   }
+//   console.log(rows)
+// }
+
+
+
+
+
+// for(let i=1;i<=10;i++)
+// {
+//   let rows=" ";
+//   for(let j=1;j<=i;j++)
+//   {
+//     rows+=" * "
+//   }
+//   console.log(rows)
+// }
+
+
+
+
+
+// for(let i=1;i<=5;i++)
+// {
+//   let rows=" ";
+//   for(let j=1;j<=i;j++)
+//   {
+//     rows+=i+"  "
+//   }
+//   console.log(rows)
+// }
+
+
+
+
+// let num=1;
+// for(let i=1;i<=4;i++)
+// {
+//   let rows=" ";
+//   for(let j=1;j<=i;j++)
+//   {
+//     rows+=num+" ";
+//     num++;
+//   }
+//   console.log(rows)
+// }
+
+
+
+
+
+// for (let i=1;i<=6;i++)
+// {
+//     let rows=" ";
+//     for(let j=1;j<=i;j++)
+//     {
+//         if(i%2==0){
+//             rows+="@  ";
+//         }
+//         else{
+//             rows+="* ";
+//         }
+//     }
+//     console.log(rows)
+// }
+
+
+
+
+// i=1;
+// while(i<=10)
+// {
+//   let rows=" "
+
+//   j=1;
+//   while(j<=10)
+//   {
+//     rows+=j+" ";
+//     j++;
+//   }
+//   console.log(rows)
+//   i++;
+// }
+
+
+
+
+// i=1;
+// while(i<=10)
+// {
+//   let rows=" "
+
+//   j=1;
+//   while(j<=i)
+//   {
+//     rows+=" * "
+//     j++;
+//   }
+//   console.log(rows)
+//   i++;
+// }
+
+
+
+
+// i=1;
+// while(i<=5)
+// {
+//   let rows=" "
+
+//   j=1;
+//   while(j<=i)
+//   {
+//     rows+=i+" "
+//     j++;
+//   }
+//   console.log(rows)
+//   i++;
+// }
+
+
+
+
+
+
+
+i=1;
+while(i<=6)
+{
+  let rows=""
+
+  j=1;
+  while(j<=i)
+  {
+  if(i%2==0)
+  {
+    rows+=" @ "
+  }
+      
+  else {
+      rows+=" * "
+    
+   }
+   j++;
+  }
+   console.log(rows)
+   i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i=1;i<=6;i++)
+// {
+//     let rows=" ";
+//     for(let j=1;j<=i;j++)
+//     {
+//         if(i%2==0){
+//             rows+="@  ";
+//         }
+//         else{
+//             rows+="* ";
+//         }
+//     }
+//     console.log(rows)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
